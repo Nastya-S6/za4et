3276914721eb9dce8dc140d48672ce9f21dc1825
Анастасия Саенкова <yfcnz.s_2003@mail.ru>
2020-07-04 11:43:30 +0400

    Добавьте файлы проекта.

---
+++ za4et/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace za4et
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Матрица:");
+            int sum = 0;
+            int[,] matrix = { { 0, 0, 1 }, { 0, 1, 1 }, { 1, 1, 1 } };
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{matrix[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
